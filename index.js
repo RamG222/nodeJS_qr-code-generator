d9e503553cf078d2b5c92bf6aad9e2345ea5b131
Ram <shriramgupta8141@gmail.com>
2024-03-25 13:24:55 +0530

    made some changes

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
 
-//1. Use the inquirer npm package to get user input.
-var url;
+//Asking for User Input 
 inquirer
   .prompt({
     message: "What is url?",
     name: "URL",
   })
+
   .then((ans) => {
+    var url;
+
     url = ans.URL;
     console.log(url);
     var qr_code = qr.image(url);
     qr_code.pipe(fs.createWriteStream("qr_image.png"));
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The files has been saved!");
+    });
   })
   .catch((e) => {
     console.log(e);
   });
-
-//2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-
-//3. Create a txt file to save the user input using the native fs node module.
---
+++ readme.md
@@ -0,0 +1,18 @@
+# QR Code Generator Using NodeJS
+
+## Description 
+Simple NodeJs Application for Generating QR code of URL entered by user and saving URL and QR code image in files
+
+## Installation 
+simply code this repo in your IDE and run "node .\index.js"
+
+## Contact Info
+GitHub profile :- https://github.com/RamG222 Email :- shriramgupta8141@gmail.com
+
+## Screenshot
+Terminal Screenshot
+
+![QR Code Generator](Terminal-ss.png)
+Output file in PNG format
+![QR Code Output](qr_image.png)
+
